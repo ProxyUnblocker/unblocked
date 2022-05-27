e0f9f44267fae963e7160911fa503a17f9bed06f
Bigfoot9999 <80417201+Bigfoot9999@users.noreply.github.com>
2022-05-27 09:07:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
-var url = prompt("Enter URL Here (example: google.com)");if (url == null) {alert('No URL Entered!')} else {document.body.innerHTML = '<iframe src="https://'+ url +'" style="position:fixed;top:0;bottom:0;left:0;right:0;width:100%;height:100%;border:none;margin:0;padding:0;z-index:999999;">Your browser does not support iframes</iframe>';alert('URL Loaded!');}
+var url = prompt("Enter URL Here (example: https://google.com)");
+if (url == null) {
+  alert('No URL Entered!')
+}
+else {
+  document.body.innerHTML = "<iframe src='url' + style='position:fixed;top:0;bottom:0;left:0;right:0;width:100%;height:100%;border:none;margin:0;padding:0;z-index:999999;'>Your browser does not support iframes</iframe>";
+  alert('URL Loaded!');
+}
